68f99b9fa365d896821e7a7e6767bb14ff8f3ca3
Marcel Blijleven <marcelblijleven@gmail.com>
2020-12-06 13:21:19 +0100

    Add day06 part one

--- day06/solution.py
+++ day06/solution.py
@@ -16,6 +16,27 @@ def groups(_input: list[str]) -> list[str]:
     yield sorted(list(set(list(group))))
 
 
+def groups_as_lists(_input: list[str]) -> list[list[str]]:
+    group = []
+    for line in _input:
+        if line:
+            group.append(line)
+        else:
+            yield group
+            group = []
+
+    yield group
+
+
+def get_all_answered(value: list[str]) -> list[str]:
+    s = set(value[0])
+
+    for i in range(1, len(value)):
+        s = s & set(value[i])
+
+    return s
+
+
 def part_one(_input):
     total = 0
 
@@ -25,4 +46,14 @@ def part_one(_input):
     return total
 
 
+def part_two(_input):
+    total = 0
+
+    for group in groups_as_lists(_input):
+        total += len(get_all_answered(group))
+
+    return total
+
+
 print(part_one(INPUT))
+print(part_two(INPUT))
--- day06/test_solution.py
+++ day06/test_solution.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 from util import get_input
-from day06.solution import groups
+from day06.solution import (
+    part_one, part_two,
+    groups, groups_as_lists, get_all_answered
+)
 
 
-class Test(TestCase):
+class TestDay06(TestCase):
     def test_groups(self):
         test_input = get_input('test_input.txt')
         retrieved_groups = list(groups(test_input))
@@ -14,3 +17,33 @@ class Test(TestCase):
         self.assertEqual(retrieved_groups[2], list('abc'))
         self.assertEqual(retrieved_groups[3], list('a'))
         self.assertEqual(retrieved_groups[4], list('b'))
+
+    def test_groups_as_lists(self):
+        test_input = get_input('test_input.txt')
+        retrieved_groups = list(groups_as_lists(test_input))
+        self.assertTrue(len(retrieved_groups), 5)
+        self.assertEqual(retrieved_groups[0], ['abc'])
+        self.assertEqual(retrieved_groups[1], ['a', 'b', 'c'])
+        self.assertEqual(retrieved_groups[2], ['ab', 'ac'])
+        self.assertEqual(retrieved_groups[3], ['a', 'a', 'a', 'a'])
+        self.assertEqual(retrieved_groups[4], ['b'])
+
+    def test_get_all_answered(self):
+        value = ['abc', 'ab', 'a']
+        result = get_all_answered(value)
+        self.assertTrue(len(result) == 1)
+        self.assertEqual(list(result), ['a'])
+
+        value = ['abc', 'bc', 'bc']
+        result = get_all_answered(value)
+        self.assertTrue(len(result) == 2)
+        self.assertTrue('b' in result)
+        self.assertTrue('c' in result)
+
+    def test_part_one(self):
+        test_input = get_input('test_input.txt')
+        self.assertEqual(part_one(test_input), 11)
+
+    def test_part_two(self):
+        test_input = get_input('test_input.txt')
+        self.assertEqual(part_two(test_input), 6)
