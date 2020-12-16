cb1062ae6cd911127bfb2b81dc01abf8dace857f
Marcel Blijleven <marcelblijleven@gmail.com>
2020-12-16 22:04:55 +0100

    improve setup_day.sh

--- setup_day.sh
+++ setup_day.sh
@@ -5,18 +5,25 @@ if [[ -z "$1" ]]; then
   exit
 fi
 
-day="day`printf %02d $1`"
+day="day$(printf %02d "$1")"
 
 if [[ -d $day ]]; then
   echo "$day already exists"
   exit
 fi
 
-mkdir $day
+mkdir "$day"
 
-touch $day/__init__.py
-touch $day/solution.py
-touch $day/input.txt
-touch $day/test_input.txt
+touch "$day/__init__.py"
+touch "$day/input.txt"
+touch "$day/test_input.txt"
 
-git add $day/*
+{
+  printf "from util import get_input\n\n"
+  printf "TEST_INPUT = get_input('test_input.txt')\n"
+  printf "INPUT = get_input('input.txt')\n\n\n"
+  printf "def part_one(_input):\n\tpass\n\n\n"
+  printf "def part_two(_input):\n\tpass\n\n"
+} >> "$day/solution.py"
+
+git add "$day/*"
